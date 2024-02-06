d218f59030be27e961efbb544f2db88dab5bae0a
marian.kuziv <mi.kuziv@gmail.com>
2024-02-06 21:59:10 +0200

    Initial commit

---
+++ src/main/java/edu/jmarkuz/retrello/ReTrelloApplication.java
@@ -0,0 +1,13 @@
+package edu.jmarkuz.retrello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReTrelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReTrelloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/jmarkuz/retrello/ReTrelloApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.jmarkuz.retrello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReTrelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
